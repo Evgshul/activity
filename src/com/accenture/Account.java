346930c193e97c05084158f1ebf82f96ca038f96
Evgeny <evgshul@gmail.com>
2019-02-22 16:42:15 +0200

    Initial commit

---
+++ src/com/accenture/Account.java
@@ -0,0 +1,9 @@
+package com.accenture;
+
+public interface Account {
+    public String getId();
+    public String getFirstName();
+    public String getLastName();
+    public String getEmail();
+    public Float getBalance();
+}
